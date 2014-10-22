4f907974279c298b41431456576252b8942ac8ad
Samuel Tonini <tonini.samuel@gmail.com>
2014-10-22 20:36:57 +0200

    implement elixir compile

--- elixir-compile.el
+++ elixir-compile.el
@@ -1,9 +1,41 @@
-;;; elixir-compile.el --- Elixir's compiler integration
+;;; elixir-compile.el --- Elixir's compile integration
 
 ;;; Commentary:
 ;;
 
+;;; Code:
 
+(defcustom elixir-compile-command "elixirc"
+  "The shell command for elixirc."
+  :type 'string
+  :group 'elixir-compile)
+
+(defvar elixir-compile-buffer-name "*elixirc*"
+  "Name of the elixir output buffer.")
+
+(defun elixir-compile-this-buffer ()
+  "Run the current buffer through elixirc."
+  (interactive)
+  (elixir-compile--file buffer-file-name))
+
+(defun elixir-compile-file (filename)
+  "Run elixir with the given `FILENAME`."
+  (interactive "Felixirc: ")
+  (elixir-compile--file (expand-file-name filename)))
+
+(defun elixir-compile--file (filename)
+  (when (not (file-exists-p filename))
+    (error "The given file doesn't exists"))
+  (elixir-compile-run (list elixir-compile-command (expand-file-name filename))))
+
+(defun elixir-compile--read-command (command)
+  (read-shell-command "elixirc command: " (concat command " ")))
+
+(defun elixir-compile-run (cmdlist)
+  "Run a elixir with `CMDLIST`."
+  (interactive (list (elixir-compile--read-command elixir-compile-command)))
+  (elixir-compilation-run (elixir-utils-build-runner-cmdlist cmdlist)
+                          elixir-compile-buffer-name))
 
 (provide 'elixir-compile)
 
