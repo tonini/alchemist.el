b0e39afbf416aa2fcdcbf7168caafc56232462a6
Samuel Tonini <tonini.samuel@gmail.com>
2014-10-23 13:46:44 +0200

    update headers

--- alchemist-eval.el
+++ alchemist-eval.el
@@ -4,6 +4,8 @@
 
 ;; Author: Samuel Tonini <tonini.samuel@gmail.com
 
+;; This file is not part of GNU Emacs.
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
@@ -16,8 +18,6 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;; This file is not part of GNU Emacs.
 
 ;;; Commentary:
 
--- alchemist.el
+++ alchemist.el
@@ -9,6 +9,8 @@
 ;; Package-Requires:
 ;; Keywords: mix, elixir, elixirc, hex
 
+;; This file is not part of GNU Emacs.
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
@@ -20,9 +22,7 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;; This file is not part of GNU Emacs.
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
