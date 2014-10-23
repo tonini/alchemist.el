a036a75036aee7b665e55b412e0d8ce30d6bcb14
Samuel Tonini <tonini.samuel@gmail.com>
2014-10-23 13:22:22 +0200

    headers for all packages

--- alchemist.el
+++ alchemist.el
@@ -1,12 +1,36 @@
 ;;; alchemist.el --- Elixir tooling integration into emacs
 
-;; Author: Samuel Tonini
-;; Maintainer: Samuel Tonini
-;; Version: 0.0.1
+;; Copyright © 2014 Samuel Tonini
+;;
+;; Author: Samuel Tonini <tonini.samuel@gmail.com>
+
+;; URL: http://www.github.com/tonini/alchemist.el
+;; Version: 0.1.0
+;; Package-Requires:
+;; Keywords: mix, elixir, elixirc, hex
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of GNU Emacs.
 
 ;;; Commentary:
-;;
 
+;;; Installation:
+
+;;; Usage:
+
+;;; Code:
 
 ;;;###autoload
 (define-minor-mode alchemist-mode
@@ -17,8 +41,7 @@ When alchemist mode is enabled, the follwing elixir modes will be loaded:
 * alchemist-compile
 * alchemist-execute
 * alchemist-eval
-* alchemist-mix
-* alchemist-iex"
+* alchemist-mix"
   nil
   ;; The indicator for the mode line.
   " alchemist"
