022fa2bb4a173a426c3640168ba7629977d205b0
Samuel Tonini <tonini.samuel@gmail.com>
2014-12-26 19:24:57 +0100

    add missing headers

--- test/test-runner.el
+++ test/test-runner.el
@@ -1,7 +1,32 @@
+;;; test-runner.el --- The test runner for the alchemist test suites
+
+;; Copyright © 2014 Samuel Tonini
+;;
+;; Author: Samuel Tonini <tonini.samuel@gmail.com>
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
 ;; Usage:
 ;;
 ;;   emacs -Q -l test/test-runner.el           # interactive mode
 ;;   emacs -batch -Q -l test/test-runner.el    # batch mode
+
+;;; Code:
 
 (let ((current-directory (file-name-directory load-file-name)))
   (setq alchemist-test-path (expand-file-name "." current-directory))
@@ -17,3 +42,7 @@
   (load test-file nil t))
 
 (ert-run-tests-batch-and-exit t)
+
+(provide 'test-runner)
+
+;;; test-runner.el ends here
