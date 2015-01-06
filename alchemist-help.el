3d9ae249574f9b1016e93b8d9f3ec8c132be8c3a
Samuel Tonini <tonini.samuel@gmail.com>
2015-01-06 21:22:05 +0100

    alchemist history should run without completion

--- alchemist-help.el
+++ alchemist-help.el
@@ -252,7 +252,7 @@ AlchemistModule.get_modules |> Enum.map &IO.puts/1
   (interactive
    (list
     (completing-read "Elixir help history: " alchemist-help-search-history nil nil "")))
-  (alchemist-help--execute search))
+  (alchemist-help--execute-without-complete search))
 
 (define-obsolete-function-alias 'alchemist-help-sexp-at-point 'alchemist-help-search-at-point "1.0.0")
 (define-obsolete-function-alias 'alchemist-help-module-sexp-at-point 'alchemist-help-search-at-point "1.0.0")
