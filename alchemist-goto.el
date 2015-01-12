961704641102ee565b281a96337188c82011f396
Samuel Tonini <tonini.samuel@gmail.com>
2015-01-12 22:40:47 +0100

    extract function from module.function expressions

---
+++ alchemist-goto.el
@@ -0,0 +1,42 @@
+;;; alchemist-goto.el ---
+
+;; Copyright © 2015 Samuel Tonini
+
+;; Author: Samuel Tonini <tonini.samuel@gmail.com
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+(defun alchemist-goto-extract-function (code)
+  "Extract function from CODE."
+  (let* ((parts (split-string code "\\."))
+         (function (car (last parts)))
+         (case-fold-search nil))
+    (when (and function
+               (string-match-p "^[a-z]+" function))
+      function)))
+
+(alchemist-goto-extract-function ":gen_tcp.accept")
+
+(provide 'alchemist-goto)
+
+;;; alchemist-goto.el ends here
