708649e9dc2b1db339b77bcf249b19ce1bf33fb0
Samuel Tonini <tonini.samuel@gmail.com>
2015-07-20 07:57:43 +0200

    bump v1.3.0 release

--- alchemist.el
+++ alchemist.el
@@ -5,7 +5,7 @@
 ;; Author: Samuel Tonini <tonini.samuel@gmail.com>
 
 ;; URL: http://www.github.com/tonini/alchemist.el
-;; Version: 1.3.0-cvs
+;; Version: 1.3.0
 ;; Package-Requires: ((emacs "24.4") (company "0.8.0"))
 ;; Keywords: languages, mix, elixir, elixirc, hex
 
@@ -60,7 +60,7 @@
   "Hook which enables `alchemist-mode'"
   (alchemist-mode 1))
 
-(defvar alchemist--version "1.3.0-cvs")
+(defvar alchemist--version "1.3.0")
 
 ;;;###autoload
 (defun alchemist-version (&optional show-version)
