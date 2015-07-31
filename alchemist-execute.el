e0c52c039e4fe75e4b10dfeb26ea2ecccb208532
Samuel Tonini <tonini.samuel@gmail.com>
2015-07-31 09:54:54 +0200

    render failing test file and stacktrace files

--- alchemist-execute.el
+++ alchemist-execute.el
@@ -26,6 +26,7 @@
 ;;; Code:
 
 (require 'alchemist-utils)
+(require 'alchemist-test-mode)
 
 (defgroup alchemist-execute nil
   "Elixir's script execution integration."
@@ -81,7 +82,15 @@
   "Run a elixir with CMDLIST."
   (interactive (list (alchemist-execute--read-command alchemist-execute-command)))
   (let ((command (alchemist-utils--build-command cmdlist)))
-      (alchemist-report-run command "alchemist-execute-report" alchemist-execute-buffer-name 'alchemist-execute-mode)))
+    (alchemist-report-run command
+                          "alchemist-execute-report"
+                          alchemist-execute-buffer-name
+                          'alchemist-execute-mode
+                          nil
+                          #'(lambda (buffer)
+                              (with-current-buffer buffer
+                                (let ((inhibit-read-only t))
+                                  (alchemist-test--render-files)))))))
 
 (provide 'alchemist-execute)
 
