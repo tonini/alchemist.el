a70d748535c3f62e872b642ff8bf784f4dd9458e
Samuel Tonini <tonini.samuel@gmail.com>
2015-08-03 19:20:24 +0200

    commentary about alchemist

--- alchemist.el
+++ alchemist.el
@@ -25,8 +25,20 @@
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
-;; Alchemist integrate Elixir's tooling into Emacs
+;;
+;;  What Does Alchemist Do For You?
+;;
+;;    Alchemist brings you all the Elixir tooling and power inside your Emacs editor.
+;;
+;;  Alchemist comes with a bunch of features, which are:
+;;
+;;    * Compile & Execution
+;;    * Inline code evaluation
+;;    * Mix integration
+;;    * Documentation lookup
+;;    * Code definition lookup
+;;    * Smart code completion
+;;    * Powerful IEx integration
 
 ;;; Code:
 
