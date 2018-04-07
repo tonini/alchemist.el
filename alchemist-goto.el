5b5282f3416c7b9dc8953bf929338d070c82836f
Aldric Giacomoni <trevoke@gmail.com>
2018-04-07 12:55:01 -0400

    bring back goto-list-symbol-def

--- alchemist-goto.el
+++ alchemist-goto.el
@@ -28,10 +28,47 @@
 (require 'xref)
 (require 'etags)
 
+(defvar alchemist-goto--symbol-list '())
+(defvar alchemist-goto--symbol-name-and-pos '())
+
 (defalias 'alchemist-goto-definition-at-point '(lambda ()
                                                  (interactive)
                                                  (xref-find-definitions (alchemist-scope-expression))))
 (defalias 'alchemist-goto-jump-back 'pop-tag-mark)
+
+(defun alchemist-goto-list-symbol-definitions ()
+  "List all symbol definitions in the current file like functions/macros/modules.
+
+It will jump to the position of the symbol definition after selection."
+  (interactive)
+  (alchemist-goto--fetch-symbol-definitions)
+  (ring-insert find-tag-marker-ring (point-marker))
+  (let* ((selected-def (completing-read "Symbol definitions:" alchemist-goto--symbol-list))
+         (position (cdr (assoc selected-def alchemist-goto--symbol-name-and-pos))))
+    (goto-char (if (overlayp position) (overlay-start position) position))))
+
+(defun alchemist-goto--fetch-symbol-definitions ()
+  (alchemist-goto--search-for-symbols "^\\s-*\\(defp?\\|defmacrop?\\|defimpl\\|defstruct\\|defmodule\\)\s.*"))
+
+(defun alchemist-goto--search-for-symbols (regex)
+  (setq alchemist-goto--symbol-list '())
+  (setq alchemist-goto--symbol-name-and-pos '())
+  (setq alchemist-goto--symbol-list-bare '())
+  (setq alchemist-goto--symbol-name-and-pos-bare '())
+  (with-current-buffer (buffer-name)
+    (save-excursion
+      (goto-char (point-min))
+      (save-match-data
+        (while (re-search-forward regex nil t)
+          (when (not (alchemist-scope-inside-string-p))
+            (when (alchemist-goto--get-symbol-from-position (car (match-data)))
+              (let* ((position (car (match-data)))
+                     (symbol (alchemist-goto--get-symbol-from-position position))
+                     (symbol-bare (alchemist-goto--get-symbol-from-position-bare position)))
+                (setq alchemist-goto--symbol-list (append alchemist-goto--symbol-list (list symbol)))
+                (setq alchemist-goto--symbol-name-and-pos (append alchemist-goto--symbol-name-and-pos (list (cons symbol position))))
+                (setq alchemist-goto--symbol-list-bare (append alchemist-goto--symbol-list-bare (list symbol-bare)))
+                (setq alchemist-goto--symbol-name-and-pos-bare (append alchemist-goto--symbol-name-and-pos-bare (list (cons symbol-bare position))))))))))))
 
 (provide 'alchemist-goto)
 
